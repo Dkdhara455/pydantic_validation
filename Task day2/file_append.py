903b2289481ea9d62daaa00ab8a6a0d191734b8d
Deepak <dharadeepakkumar2000@gmail.com>
2025-08-12 09:27:23 +0530

    pydantic validation

--- Task day2/file_append.py
+++
@@ -1,2 +0,0 @@
-file=open("Task day2\ file_write.txt","a")
-file.write("line3-Dhara\n")
--- Task day2/file_write.py
+++
@@ -1,5 +0,0 @@
-file = open("Task day2\ file_write.txt", "w")
-file.write("Hello, world!\n")
-file.writelines(["Line1-Deepak\n", "Line2-Kumar\n"])
-file.close()
-
